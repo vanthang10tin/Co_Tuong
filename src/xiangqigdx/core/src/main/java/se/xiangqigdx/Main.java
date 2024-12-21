d06fcb905d71996965078efd9da9d013f3bafcfa
uxlem <133137594+uxlem@users.noreply.github.com>
2024-12-21 22:22:19 +0700

    qq

--- src/xiangqigdx/core/src/main/java/se/xiangqigdx/Main.java
+++ src/xiangqigdx/core/src/main/java/se/xiangqigdx/Main.java
@@ -6,17 +6,16 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
-    private FitViewport viewport;
+
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("Xiangqi_board.svg.png");
+        image = new Texture("libgdx.png");
     }
 
     @Override
