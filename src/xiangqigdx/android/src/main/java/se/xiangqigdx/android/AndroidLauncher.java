57af7c4642990f4c1ab60dc9a0f30091e9a9a306
uxlem <133137594+uxlem@users.noreply.github.com>
2024-12-22 09:41:52 +0700

    xoay ngang dọc màn android

--- src/xiangqigdx/android/src/main/java/se/xiangqigdx/android/AndroidLauncher.java
+++ src/xiangqigdx/android/src/main/java/se/xiangqigdx/android/AndroidLauncher.java
@@ -12,7 +12,7 @@ public class AndroidLauncher extends AndroidApplication {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
-        configuration.useImmersiveMode = true; // Recommended, but not required.
+        configuration.useImmersiveMode = false; // Recommended, but not required.
         initialize(new Main(), configuration);
     }
 }
